257efc9561e7e61064e6aa46a242683e947d67ca
adhie1337 <adhie@inf.elte.hu>
2017-05-25 21:12:59 +0200

    Initial commit

---
+++ Adhie.ApplicationWithNotifyIcon/Application.cs
@@ -0,0 +1,99 @@
+﻿namespace Adhie.ApplicationWithNotifyIcon
+{
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+    
+    /// <summary>
+    /// An <see cref="ApplicationContext"/> type that adds a <see cref="NotifyIcon"/> functionality.
+    /// </summary>
+    /// <typeparam name="TForm">The type of the main <see cref="Form"/> of the application.</typeparam>
+    public class Application<TForm> : ApplicationContext where TForm : Form
+    {
+        private readonly Func<TForm> _formFactory;
+        private readonly NotifyIcon _notifyIcon;
+        private readonly ContextMenuStrip _contextMenu;
+        private readonly ToolStripItem _toggle;
+        private readonly ToolStripItem _exit;
+
+        private TForm _mainWindow;
+        
+        private TForm MainWindow
+        {
+            get
+            {
+                if (this._mainWindow == null)
+                {
+                    this._mainWindow = _formFactory();
+                    this._mainWindow.FormClosed += MainWindow_FormClosed;
+                }
+
+                return this._mainWindow;
+            }
+        }
+
+        internal Application(Func<TForm> formFactory, string title, Icon icon)
+            : base()
+        {
+            this._formFactory = formFactory;
+            this._toggle = new ToolStripMenuItem();
+            this._toggle.Size = new Size(220, 22);
+            this._toggle.Text = "Toggle " + title;
+            this._toggle.Click += new EventHandler(this.Toggle_Click);
+
+            this._exit = new ToolStripMenuItem();
+            this._exit.Size = new Size(220, 22);
+            this._exit.Text = "Exit";
+            this._exit.Click += new EventHandler(this.Exit_Click);
+
+            this._contextMenu = new ContextMenuStrip();
+            this._contextMenu.Items.AddRange(new ToolStripItem[] {
+                this._toggle,
+                this._exit});
+            this._contextMenu.Size = new Size(221, 48);
+
+            this._notifyIcon = new NotifyIcon();
+            this._notifyIcon.ContextMenuStrip = this._contextMenu;
+            this._notifyIcon.Icon = icon;
+            this._notifyIcon.Text = title;
+            this._notifyIcon.Visible = true;
+            this._notifyIcon.MouseDoubleClick += new MouseEventHandler(this.NotifyIcon_MouseDoubleClick);
+        }
+
+        private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Toggle_Click(sender, e);
+        }
+
+        private void Toggle_Click(object sender, EventArgs e)
+        {
+            if (this.MainWindow.WindowState == FormWindowState.Minimized)
+            {
+                this.MainWindow.Show();
+                this.MainWindow.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                this.MainWindow.WindowState = FormWindowState.Minimized;
+                this.MainWindow.Hide();
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            if (this._mainWindow != null)
+            {
+                this.MainWindow.Close();
+            }
+
+            this._notifyIcon.Dispose();
+            this.ExitThread();
+        }
+
+        private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this._notifyIcon.Dispose();
+            this.ExitThread();
+        }
+    }
+}
---
+++ Adhie.ApplicationWithNotifyIcon/Factory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Adhie.ApplicationWithNotifyIcon
+{
+    /// <summary>
+    /// A factory class that creates <see cref="Application{TForm}"/> instances.
+    /// </summary>
+    public static class ApplicationFactory
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="Application{TForm}"/> class. Sets up the necessary controls,
+        /// like the notify icon and it's context menu, with the toggle and exit menu items.
+        /// </summary>
+        /// <typeparam name="TForm">The type of the main form.</typeparam>
+        /// <param name="factory">Factory function that creates the form instance.</param>
+        /// <param name="title">Title of the application.</param>
+        /// <param name="icon">Icon of the application.</param>
+        /// <returns>The created instance of the application.</returns>
+        public static Application<TForm> Create<TForm>(Func<TForm> factory, string title, Icon icon) where TForm : Form
+        {
+            return new Application<TForm>(factory, title, icon);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Application{TForm}"/> class. Sets up the necessary controls,
+        /// like the notify icon and it's context menu, with the toggle and exit menu items.
+        /// </summary>
+        /// <typeparam name="TForm">The type of the main form.</typeparam>
+        /// <param name="title">Title of the application.</param>
+        /// <param name="icon">Icon of the application.</param>
+        /// <returns>The created instance of the application.</returns>
+        public static Application<TForm> Create<TForm>(string title, Icon icon) where TForm : Form, new()
+        {
+            return new Application<TForm>(() => new TForm(), title, icon);
+        }
+    }
+}
